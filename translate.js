8f2983d852e68c01987666db43b8269c22c3422f
C210102 <c210102@ccg.ac.jp>
2022-03-25 16:24:54 +0900

    up_translate

---
+++ translate.js
@@ -0,0 +1,215 @@
+function japan() {
+    let header_title = document.querySelector('.header_title');
+    header_title.textContent = '履歴書';
+    header_title.style.letterSpacing = '12px';
+
+    let header_text = document.querySelector('.header_text');
+    header_text.textContent = 'ウェブデザイナー';
+    header_text.style.letterSpacing = '10px';
+
+    let infor_translate = document.querySelector('.infor_translate');
+    infor_translate.textContent = '個人情報';
+    infor_translate.style.letterSpacing = '5px';
+
+    let name_translate = document.querySelector('.name_translate');
+    name_translate.textContent = '氏名 : VU DUY CUONG';
+
+    let date_translate = document.querySelector('.date_translate');
+    date_translate.textContent = '生年月日 : 1997 / 02 / 02';
+
+    let add_translate = document.querySelector('.add_translate');
+    add_translate.textContent = '現在所 : 埼玉県志木市本町';
+
+    let contact_translate = document.querySelector('.contact_translate');
+    contact_translate.textContent = '連絡先';
+    contact_translate.style.letterSpacing = '5px';
+
+    let skill_translate = document.querySelector('.skill_translate');
+    skill_translate.textContent = '連絡先';
+    skill_translate.style.letterSpacing = '5px';
+
+    let edu_translate = document.querySelector('.edu_translate');
+    edu_translate.textContent = '学歴';
+    edu_translate.style.letterSpacing = '5px';
+    edu_translate.style.fontWeight = '600';
+
+    let CTB_translate = document.querySelector('.CTB_translate');
+    CTB_translate.textContent = 'CHUYEN THAI BINH 高等学校';
+    CTB_translate.style.letterSpacing = '4px';
+
+    let HN_translate = document.querySelector('.HN_translate');
+    HN_translate.textContent = 'ハノイ師範大学';
+    HN_translate.style.letterSpacing = '4px';
+
+    let Aka_translate = document.querySelector('.Aka_translate');
+    Aka_translate.textContent = '赤門会日本語学校';
+    Aka_translate.style.letterSpacing = '4px';
+
+    let CO_translate = document.querySelector('.CO_translate');
+    CO_translate.textContent = '中央情報専門学校';
+    CO_translate.style.letterSpacing = '4px';
+
+    let web_translate = document.querySelector('.web_translate');
+    web_translate.textContent = 'IT/web学科';
+    web_translate.style.letterSpacing = '4px';
+
+    let quali_translate = document.querySelector('.quali_translate');
+    quali_translate.textContent = '資格';
+    quali_translate.style.letterSpacing = '5px';
+    quali_translate.style.fontWeight = '600';
+
+    let bache_translate = document.querySelector('.bache_translate');
+    bache_translate.textContent = '学士号';
+    bache_translate.style.letterSpacing = '4px';
+
+    let good_translate = document.querySelector('.good_translate');
+    good_translate.textContent = '学位分類：良い';
+    good_translate.style.letterSpacing = '4px';
+
+    let ibut_translate = document.querySelector('.ibut_translate');
+    ibut_translate.textContent = 'インターネット ベーシック ユーザー テスト';
+    ibut_translate.style.letterSpacing = '2px';
+
+    let gold_translate = document.querySelector('.gold_translate');
+    gold_translate.textContent = 'ゴールド認定証';
+    gold_translate.style.letterSpacing = '4px';
+
+    let N_translate = document.querySelector('.N_translate');
+    N_translate.textContent = '日本語能力認定書';
+    N_translate.style.letterSpacing = '4px';
+
+    let n2_translate = document.querySelector('.n2_translate');
+    n2_translate.textContent = 'レベル N2';
+    n2_translate.style.letterSpacing = '4px';
+    n2_translate.style.fontStyle = 'normal';
+
+    let made_translate = document.querySelector('.made_translate');
+    made_translate.textContent = '作られた製品';
+    made_translate.style.letterSpacing = '5px';
+    made_translate.style.fontWeight = '600';
+
+    let title_translate_1 = document.querySelector('.title_translate_1');
+    title_translate_1.textContent = '学士号';
+    title_translate_1.style.letterSpacing = '5px';
+
+    let title_translate_2 = document.querySelector('.title_translate_2');
+    title_translate_2.textContent = 'インターネット ベーシック ユーザー テスト';
+    title_translate_2.style.letterSpacing = '5px';
+
+    let title_translate_3 = document.querySelector('.title_translate_3');
+    title_translate_3.textContent = '日本語能力認定書';
+    title_translate_3.style.letterSpacing = '5px';
+
+    let title_translate_4 = document.querySelector('.title_translate_4');
+    title_translate_4.textContent = '2021年度の成績表';
+    title_translate_4.style.letterSpacing = '5px';
+}
+
+
+
+function english() {
+    let header_title = document.querySelector('.header_title');
+    header_title.textContent = 'CURRICULUM VITAE';
+    header_title.style.letterSpacing = '6px';
+
+    let header_text = document.querySelector('.header_text');
+    header_text.textContent = 'Web Designer';
+    header_text.style.letterSpacing = '4px';
+
+    let infor_translate = document.querySelector('.infor_translate');
+    infor_translate.textContent = 'Information';
+    infor_translate.style.letterSpacing = '0px';
+
+    let name_translate = document.querySelector('.name_translate');
+    name_translate.textContent = 'Name : VU DUY CUONG';
+
+    let date_translate = document.querySelector('.date_translate');
+    date_translate.textContent = 'Date of birth : 1997 / 02 / 02';
+
+    let add_translate = document.querySelector('.add_translate');
+    add_translate.textContent = 'Address : Honcho, Shiki-shi, Saitama-ken, 353-0004 JAPAN';
+
+    let contact_translate = document.querySelector('.contact_translate');
+    contact_translate.textContent = 'Contact';
+    contact_translate.style.letterSpacing = '0px';
+
+    let skill_translate = document.querySelector('.skill_translate');
+    skill_translate.textContent = 'Skills';
+    skill_translate.style.letterSpacing = '0px';
+
+    let edu_translate = document.querySelector('.edu_translate');
+    edu_translate.textContent = 'Education';
+    edu_translate.style.letterSpacing = '0px';
+    edu_translate.style.fontWeight = '500';
+
+    let CTB_translate = document.querySelector('.CTB_translate');
+    CTB_translate.textContent = 'CHUYEN THAI BINH High School';
+    CTB_translate.style.letterSpacing = '0px';
+
+    let HN_translate = document.querySelector('.HN_translate');
+    HN_translate.textContent = 'Hanoi National University of Education';
+    HN_translate.style.letterSpacing = '0px';
+
+    let Aka_translate = document.querySelector('.Aka_translate');
+    Aka_translate.textContent = 'Akamonkai Japanese Language School';
+    Aka_translate.style.letterSpacing = '0px';
+
+    let CO_translate = document.querySelector('.CO_translate');
+    CO_translate.textContent = 'Chuo Computer & Communication College';
+    CO_translate.style.letterSpacing = '0px';
+
+    let web_translate = document.querySelector('.web_translate');
+    web_translate.textContent = 'IT・WEB Department';
+    web_translate.style.letterSpacing = '0px';
+    
+    let quali_translate = document.querySelector('.quali_translate');
+    quali_translate.textContent = 'Qualification';
+    quali_translate.style.letterSpacing = '0px';
+    quali_translate.style.fontWeight = '500';
+
+    let bache_translate = document.querySelector('.bache_translate');
+    bache_translate.textContent = 'The Degree of Bachelor';
+    bache_translate.style.letterSpacing = '0px';
+
+    let good_translate = document.querySelector('.good_translate');
+    good_translate.textContent = 'Degree classification: Good';
+    good_translate.style.letterSpacing = '0px';
+
+    let ibut_translate = document.querySelector('.ibut_translate');
+    ibut_translate.textContent = 'Certificate of Internet Basic user Test (iBut)';
+    ibut_translate.style.letterSpacing = '0px';
+
+    let gold_translate = document.querySelector('.gold_translate');
+    gold_translate.textContent = 'Gold Certificate';
+    gold_translate.style.letterSpacing = '0px';
+
+    let N_translate = document.querySelector('.N_translate');
+    N_translate.textContent = 'Certificate of Japanese Language Proficiency';
+    N_translate.style.letterSpacing = '0px';
+
+    let n2_translate = document.querySelector('.n2_translate');
+    n2_translate.textContent = 'Level N2';
+    n2_translate.style.letterSpacing = '0px';
+    n2_translate.style.fontStyle = 'italic';
+
+    let made_translate = document.querySelector('.made_translate');
+    made_translate.textContent = 'Products Made';
+    made_translate.style.letterSpacing = '0px';
+    made_translate.style.fontWeight = '500';
+
+    let title_translate_1 = document.querySelector('.title_translate_1');
+    title_translate_1.textContent = 'The Degree of Bachelor';
+    title_translate_1.style.letterSpacing = '0px';
+
+    let title_translate_2 = document.querySelector('.title_translate_2');
+    title_translate_2.textContent = 'Certificate of Internet Basic user Test';
+    title_translate_2.style.letterSpacing = '0px';
+
+    let title_translate_3 = document.querySelector('.title_translate_3');
+    title_translate_3.textContent = 'Certificate of Japanese Language Proficiency';
+    title_translate_3.style.letterSpacing = '0px';
+
+    let title_translate_4 = document.querySelector('.title_translate_4');
+    title_translate_4.textContent = '2021 report card';
+    title_translate_4.style.letterSpacing = '0px';
+}
